b281de3bcd0cdadace31fe9fa27a1e8b3600f38c
Krishna Gharti <krishnagharti688@gmail.com>
2026-08-05 15:20:46 +0545

    login update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-5xl font-bold text-blue-600">
-        HydraNexa Admin
-      </h1>
-    </div>
-  );
+  redirect("/login");
 }
