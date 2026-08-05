6fef28772c7258bd45368b11f39f91e58c6406d5
Krishna Gharti <krishnagharti688@gmail.com>
2026-08-05 15:37:24 +0545

    test dashboard

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,22 +1,3 @@
-import Sidebar from "@/components/dashboard/Sidebar";
-import Navbar from "@/components/dashboard/Navbar";
-
 export default function DashboardLayout({ children }) {
-    return (
-        <div className="flex min-h-screen bg-[#F4F7FB]">
-
-            <Sidebar />
-
-            <div className="flex flex-1 flex-col">
-
-                <Navbar />
-
-                <main className="flex-1 p-6">
-                    {children}
-                </main>
-
-            </div>
-
-        </div>
-    );
+    return <>{children}</>;
 }
